7ec93813888f518b80ca0ab4a5c1d59b3a04befc
mayur111999 <mayur9fe@gmail.com>
2020-01-14 10:12:20 +0530

    Create object using class.forname

---
+++ Assign_ClassName/src/main_1.java
@@ -0,0 +1,25 @@
+//create object using class.forname
+public class main_1 {
+	int a=10;
+    public static void main(String[] args) 
+    { 
+        try
+        { 
+            Class cls = Class.forName("main_1"); //Loads the class in java
+            main_1 obj =  (main_1) cls.newInstance(); //using newInstance Method to crate object
+            System.out.println(obj.a); 
+        } 
+        catch (ClassNotFoundException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        catch (InstantiationException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+        catch (IllegalAccessException e) 
+        { 
+            e.printStackTrace(); 
+        } 
+}
+}

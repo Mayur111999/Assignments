8317fd4321ef1342d03191096577b96af7f7e7e2
Mayur111999 <59784902+Mayur111999@users.noreply.github.com>
2020-01-15 20:38:49 +0530

    Update main_1.java

--- Assignment_Multithread/src/main_1.java
+++ Assignment_Multithread/src/main_1.java
@@ -42,6 +42,12 @@ public class main_1 {
 }
 
 /*OUTPUT: - 
+
+create multiple connection without closing it to see Errors
+
+
+
+
  * Wed Jan 15 15:00:06 IST 2020 WARN: Establishing SSL connection without server's identity verification is not recommended. According to MySQL 5.5.45+, 5.6.26+ and 5.7.6+ requirements SSL connection must be established by default if explicit option isn't set. For compliance with existing applications not using SSL the verifyServerCertificate property is set to 'false'. You need either to explicitly disable SSL by setting useSSL=false, or set useSSL=true and provide truststore for server certificate verification.
 Wed Jan 15 15:00:06 IST 2020 WARN: Establishing SSL connection without server's identity verification is not recommended. According to MySQL 5.5.45+, 5.6.26+ and 5.7.6+ requirements SSL connection must be established by default if explicit option isn't set. For compliance with existing applications not using SSL the verifyServerCertificate property is set to 'false'. You need either to explicitly disable SSL by setting useSSL=false, or set useSSL=true and provide truststore for server certificate verification.
 Exception is caught Data source rejected establishment of connection,  message from server: "Too many connections"

b4108ac86cad7559e63044b3fdecd5a807127c96
mayur111999 <mayur9fe@gmail.com>
2020-01-15 21:05:57 +0530

    finish

--- AdapterPattern2/src/main1.java
+++ AdapterPattern2/src/main1.java
@@ -9,15 +9,29 @@ class main1
         // behaves like toy duck 
         ToyDuck birdAdapter = new BirdAdapter(sparrow); 
   
-        System.out.println("Sparrow..."); 
+        System.out.println("Run Simple Sparrow Implements Bird..."); 
         sparrow.fly(); 
         sparrow.makeSound(); 
   
-        System.out.println("ToyDuck..."); 
+        System.out.printf("\nSimple ToyDuck...\n"); 
         toyDuck.squeak(); 
   
         // toy duck behaving like a bird  
-        System.out.println("BirdAdapter..."); 
+        System.out.printf("\n\nBirdAdapter...\n"); 
         birdAdapter.squeak(); 
     } 
 } 
+
+/*OUTPUT: - 
+Run Simple Sparrow Implements Bird...
+Flying
+Chirp Chirp
+
+Simple ToyDuck...
+Squeak
+
+
+BirdAdapter...
+Chirp Chirp
+
+*/
